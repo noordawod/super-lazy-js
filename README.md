b40b5370c28813ad583c755b79dc77c15d2227cf
Noor Dawod <github@fineswap.com>
2014-05-01 13:00:42 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,191 @@
+Lazy JS
+=======
+
+A minimalist, powerful and asynchronous lazy loader for JavaScript files. In order to use Lazy JS,
+no changes need to be made to the original scripts. If used correctly, however, Lazy JS could
+drastically speed up loading of external JavaScript files while not locking up the UI.
+
+The minified size of the simple version is around 3Kb (1.5Kb gzipp'ed or [Zopfli][zopfli]'ed),
+and with only a few extra bytes, Lazy JS may also scan the document for inline scripts'
+meta-data to load.
+
+Lazy JS comes in two flavors for your consideration:
+
+1. **Normal version**:  
+   Lazy JS is defined and a single variable is exposed to global scope for use.
+2. **Automatic version**:  
+   In addition to the above, the automatic version has a routine that scans the DOM looking
+   for &lt;script&gt; elements. Once found, these are checked for meta-data about which scripts to
+   load.
+
+How Lazy JS works
+-----------------
+
+A well-behaved Web app should load external scripts on-demand and as-needed. This is especially
+true for mobile devices where resources are jealously kept to a minimum. Furthermore, any script
+that is loaded, although not actively running, is still taking up resources -- particularly
+memory and stack.
+
+Due to these understandings, Lazy JS was built with the fundamental principle that an app may be
+divided into separate parts, each part relies on the one preceding it. While these parts rely 
+on one another, one part (called *chunk* in Lazy JS) includes one or more, unrelated scripts
+that load *asynchronously*.
+
+This technique allows you to divide the app's scripts into chunks (that load *synchronously*,
+one after the other) and each chunk into various scripts (that load *asynchronously*, in
+parallel). Each chunk has a unique identifier which is sent to the various configurable callbacks.
+
+Documentation
+-------------
+
+Both versions introduce the variable **LazyJS** to global scope which is basically an Object.
+Configuration options are passed as an Array (to preserve order of appearance), each two items
+in the array correspond to a *key* and its associated *value*.
+
+Recognized keys are one or more of the following:
+
+- <code>prefix</code>: *String*, defaults to *'lazy'*  
+  The prefix is used by Lazy JS to formulate CSS class names to add to the
+  main HTML element during loading of the scripts. For example, when loading a *'libs'* chunk,
+  Lazy JS adds a class name *'lazy-libs'* to the HTML element. Once loading has finished
+  successfully, the class name is replaced with *'lazy-libs-loaded'*.
+
+- <code>canStart()</code>: *Callback* *(default handler returns true always)*  
+  Before beginning to load any of the scripts, Lazy JS calls this function. If the function
+  returns *true*, Lazy JS starts the loading process, but not before that. Lazy JS will continue
+  to check this function (for 10 seconds) until it returns *true*. While loading is in progress,
+  the class name *'lazy-loading'* (prefix may change, of course) is added to main HTML element.
+
+- <code>onProgress(*chunkId*, *script*, *timeElapsed*)</code>: *Callback*  
+  Each time a script is successfully loaded by Lazy JS, this function is called, along with its
+  associated chunk ID, the script's URL and time it took to load it.
+
+- <code>onChunk(*chunkId*, *timeElapsed*)</code>: *Callback*  
+  Each time a bunch of scripts belonging to the same chunk are successfully loaded by Lazy JS,
+  this function is called, along with the chunk ID and the overall time required to load them.
+
+- <code>onSuccess()</code>: *Callback*  
+  When all chunks have been successfully loaded, this function is called. The function is called
+  only once.
+
+- <code>onError(*chunkId*, *script*)</code>: *Callback*  
+  When any script, in any chunk, fails to load, this function is called (only once). The
+  associated chunk ID and the script's URL are passed as well. Once an error occurs in the loading
+  process, Lazy JS cancels all pending operations.  
+  A special case: When Lazy JS doesn't get *true* for 10 seconds from <code>canStart()</code>
+  handler, <code>onError()</code> handler is called with *chunkId* equal to '*'.
+
+Any unrecognized key is considered a *chunk ID*, and its value may either be an Array of scripts,
+a String holding a list of scripts separated by spaces or a String holding one script entry.
+
+Once instantiated, a Lazy JS object may call these methods to further tune its workings:
+
+- .<code>canStart(*[handler]*)</code>  
+  .<code>onProgress(*[handler]*)</code>  
+  .<code>onChunk(*[handler]*)</code>  
+  .<code>onSuccess(*[handler]*)</code>  
+  .<code>onError(*[handler]*)</code>  
+  A series of methods to set a new callback, or get the current one, for the appropriate event.
+
+- .<code>add(*chunkId*, *scripts*)</code>  
+  Add a new chunk of scripts identified by the unique ID *chunkId*. *scripts* may be passed as
+  an Array of Strings, or a String holding scripts separated by a space, or a single String entry.
+  
+- .<code>load()</code>  
+  Kick-start the loading sequence. load() will call <code>canStart()</code> handler for up to
+  10 seconds waiting for a *true* response. Once received, loading starts.
+
+Examples
+--------
+
+This is a simple example of using Lazy JS normal version:
+
+    <!DOCTYPE html>
+    <html>
+      <head>
+        ...
+      </head>
+      <body>
+        ...
+        <script type="text/javascript" src="/path/to/lazy.js"></script>
+        <script type="text/javascript">
+          // Use a closure to sandbox the logic.
+          (function(LazyJS) {
+            (new LazyJS([
+              'prefix', 'myapp',
+
+              'onError', function() {
+              },
+
+              // Chunk 'libs' includes 4 scripts, which load asynchronously (as an Array).
+              'libs', ['json2.js', 'jquery.js', 'sass.js', 'md5.js'],
+
+              // After 'libs' loads, chunk 'plugins' starts loading (as a String).
+              'plugins', 'jquery-ui.js plugin.js another-plugin.js',
+
+              // Finally, this chunk includes just one script to load (one entry).
+              'app', 'launch.js'
+            ])).load();
+          })(LazyJS);
+        </script>
+      </body>
+    </html>
+
+The same can done in just one line using Lazy JS automatic version:
+
+    <!DOCTYPE html>
+    <html>
+      <head>
+        ...
+        <script type="text/javascript">
+          function lazyCanStart() { return true; }
+          function lazyOnProgress(chunkId, scriptUrl, timeElapsed) { ... }
+          function lazyOnChunk(chunkId, timeElapsed) { ... }
+          function lazyOnSuccess() { ... }
+          function lazyOnError(chunkId, scriptUrl) { ... }
+        </script>
+      </head>
+      <body>
+        ...
+      <script data-can-start="lazyCanStart"
+              data-on-progress="lazyOnProgress"
+              data-on-chunk="lazyOnChunk"
+              data-on-success="lazyOnSuccess"
+              data-on-error="lazyOnError"
+              data-prefix="myapp"
+              data-libs="json2.js jquery.js sass.js md5.js"
+              data-plugins="jquery-ui.js plugin.js another-plugin.js"
+              data-app="launch.js"
+              type="text/javascript" src="lazy-auto.js"></script>
+      </body>
+    </html>
+
+A note about "DOM Ready"
+------------------------
+
+Lazy JS does not wait till DOM's ready; once its <code>load()</code> method is called, and
+<code>canStart()</code> returns *true*, loading initiates. Waiting for the DOM is left to
+your discretion.
+
+I could have incorporated the necessary code to achieve this (my [DOM Ready][dom-ready] closure
+does just that), but I decided against it since Lazy JS may be included in various environments
+and apps.
+
+In any case, I would recommend inserting any &lt;script&gt; elements just before the closing 
+&lt;/body&gt; element, for two main reasons:
+
+1. All HTML code would be fully loaded at that point;
+2. The UI will not lock up waiting for &lt;script&gt;'s to load and be parsed.
+
+This will most likely ensure that your app doesn't even need to wait for a DOM ready event, and
+you may then kick-start Lazy JS's loading frenzy at once.
+
+Terms of Use
+------------
+
+[MIT License][mitlicense]
+
+[mitlicense]: http://en.wikipedia.org/wiki/MIT_License        "MIT License"
+[modernizr]:  http://modernizr.com/                           "Modernizr"
+[dom-ready]:  https://github.com/fineswap/dom-ready           "DOM Ready"
+[zopfli]:     https://code.google.com/p/zopfli/               "Zopfli"
---
+++ lazy.js
@@ -0,0 +1,422 @@
+/*!
+ * Lazy JS v1.0.6
+ * https://github.com/fineswap/lazy-js
+ *
+ * Copyright (C) 2013-2014 Fineswap Blog & App
+ * http://fineswap.com/
+ *
+ * Released under the MIT license
+ * http://en.wikipedia.org/wiki/MIT_License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+;var LazyJS = (function(window, Object, Array, Date, NULL, undefined) {
+  'use strict';
+
+  // Mentioned few times in the code, declare them here to gain better minification.
+  var TRUE = !0,
+      FALSE = !1,
+      DOCUMENT = 'document',
+      ELEMENT = 'Element',
+      HEAD = 'head',
+      LOADED = 'loaded',
+      PROTOTYPE = 'prototype',
+      FIRSTCHILD = 'firstChild',
+      READYSTATE = 'readyState',
+      CALL = 'call',
+      SHIFT = 'shift',
+      PUSH = 'push',
+      SUBSTR = 'substr',
+      LOAD = 'load',
+      ADD = 'add',
+      LENGTH = 'length',
+      HANDLERS = 'handlers',
+      QUEUE = 'queue',
+      PREFIX = 'prefix',
+      ONLOAD = 'on' + LOAD,
+      LOADING = LOAD + 'ing',
+      ONREADYSTATECHANGE = 'onreadystatechange',
+      CLASSNAME = 'className',
+      REMOVECHILD = 'removeChild',
+      GETELEMENTSBYTAGNAME = 'get' + ELEMENT + 'sByTagName',
+      TOLOWERCASE = 'toLowerCase',
+      SLICE = 'slice',
+      REPLACE = 'replace',
+      CANSTART = 'canStart',
+      ONSUCCESS = 'onSuccess',
+      ONPROGRESS = 'onProgress',
+      ONCHUNK = 'onChunk',
+      ONERROR = 'onError',
+
+      document = window.document,
+      html = document[GETELEMENTSBYTAGNAME]('html')[0] || document[DOCUMENT + ELEMENT],
+      head = document[HEAD] || document[GETELEMENTSBYTAGNAME](HEAD)[0],
+
+      noop = function() {},
+
+      is = function(object, type, classType) {
+        if(undefined === object || NULL === object)
+          return FALSE;
+        classType = Object[PROTOTYPE].toString[CALL](object)[SLICE](8, -1)[TOLOWERCASE]();
+        return classType === type[TOLOWERCASE]();
+      },
+
+      isFunction = function(handler) {
+        return is(handler, 'function');
+      },
+
+      isArray = function(object) {
+        return is(object, 'array');
+      },
+
+      cloneArray = function(array, startFrom) {
+        return Array[PROTOTYPE].slice[CALL](array, startFrom || 0);
+      },
+
+      defaultStartHandler = function() {
+        return TRUE;
+      },
+
+      defaultErrorHandler = function(chunkId, src) {
+        throw 'Unable to load: ' + src + ' [' + chunkId + ']';
+      },
+
+      // Fire the specified callback inside a Lazy JS object.
+      // 'this' points to the target LazyJS instantiated object.
+      fireCallback = function(handlerId) {
+        var self = this, callback = self[HANDLERS][handlerId];
+        if(is(callback, 'string'))
+          callback = window[callback];
+        return isFunction(callback)
+          ? callback.apply(self, cloneArray(arguments, 1))
+          : undefined;
+      },
+
+      trim = function(string) {
+        return !string ? '' : string[REPLACE](/(^\s+|\s+$)/g, '');
+      },
+
+      addClass = function(cls) {
+        html[CLASSNAME] = trim(html[CLASSNAME] + ' ' + cls);
+      },
+
+      removeClass = function(cls) {
+        html[CLASSNAME] = trim(html[CLASSNAME][REPLACE](new RegExp('\s*' + cls + '\s*', 'g'), ' '));
+      },
+
+      getTime = function() {
+        return +new Date;
+      },
+
+      // Create a new script element, and load a script from the provided URL.
+      // 'this' points to the target LazyJS instantiated object.
+      loadScript = function(src, successCallback, errorCallback) {
+        var self = this,
+            script = document['create' + ELEMENT]('script'),
+            timer,
+            onLoad = function() {
+              script[ONLOAD] = NULL;
+              head[REMOVECHILD](script);
+              successCallback(src, getTime() - timer);
+            },
+            onError = function() {
+              self.quit = TRUE;
+              script[ONLOAD] = script[ONREADYSTATECHANGE] = NULL;
+              head[REMOVECHILD](script);
+              errorCallback(src);
+            };
+
+        if(!self.quit) {
+          script.src = src;
+          script.async = TRUE;
+          script.type = 'text/javascript';
+          script.onerror = onError;
+          script[ONLOAD] = onLoad;
+          script[ONREADYSTATECHANGE] = function() {
+            if(LOADED === this[READYSTATE] || 'complete' === this[READYSTATE]) {
+              script[ONREADYSTATECHANGE] = NULL;
+              onLoad();
+            }
+          };
+
+          try {
+            timer = getTime();
+            if(head[FIRSTCHILD])
+              head.insertBefore(script, head[FIRSTCHILD]);
+            else
+              head.appendChild(script);
+          } catch (e) {
+            onError();
+          }
+        }
+      },
+
+      // Load a bunch of scripts asynchronously.
+      // 'this' points to the target LazyJS instantiated object.
+      loadAsync = function(chunkId, queue, successCallback, errorCallback) {
+        var self = this,
+            size = queue[LENGTH],
+            loaded = 0,
+            src,
+            timeElapsed = 0,
+            removeIdClass = function() {
+              // Remove class from HTML element.
+              removeClass(self[PREFIX] + '-' + chunkId);
+            },
+            onSuccess = function(src, time) {
+              loaded++;
+              fireCallback[CALL](self, ONPROGRESS, chunkId, src, time);
+              timeElapsed += time;
+              if(loaded === size) {
+                removeIdClass();
+                addClass(self[PREFIX] + '-' + chunkId + '-' + LOADED);
+                successCallback(chunkId, timeElapsed);
+              }
+            },
+            onError = function(src) {
+              // Call the error handler only once.
+              removeIdClass();
+              errorCallback(chunkId, src);
+            };
+
+        // Add class to the HTML element.
+        addClass(self[PREFIX] + '-' + chunkId);
+
+        // Go over list of scripts and load them one after the other.
+        while(!!(src = queue[SHIFT]()))
+          loadScript[CALL](self, src, onSuccess, onError);
+      },
+
+      // Load a bunch of scripts synchronously.
+      // 'this' points to the target LazyJS instantiated object.
+      loadSync = function() {
+        var self = this,
+            fired,
+            queue,
+            removeIdClass = function() {
+              // Remove class from HTML element.
+              removeClass(self[PREFIX] + '-' + LOADING);
+            },
+            errorCallback = function(chunkId, src) {
+              if(!fired) {
+                // Call the error handler only once.
+                fired = TRUE;
+                removeIdClass();
+                fireCallback[CALL](self, ONERROR, chunkId, src);
+              }
+            },
+            loadNextChunk = function(chunkId, timeElapsed) {
+              // Load them asynchronously.
+              if(!self.quit) {
+                // Fire a callback to update the app.
+                if(chunkId)
+                  fireCallback[CALL](self, ONCHUNK, chunkId, timeElapsed);
+
+                // Get next bunch of scripts.
+                queue = self[QUEUE][SHIFT]();
+
+                // If there are more scripts, load them now.
+                if(queue && queue[LENGTH])
+                  loadAsync[CALL](self, queue[0], queue[1], loadNextChunk, errorCallback);
+                else {
+                  // Finished loading all scripts!
+                  removeIdClass();
+
+                  // Call the onSuccess handler.
+                  fireCallback[CALL](self, ONSUCCESS);
+
+                  // Delete this property, we don't need it anymore.
+                  self[ONLOAD] = undefined;
+                }
+              }
+            };
+
+        // Pull for 10 seconds only (quite a long time in Web realm, anyway.)
+        if(getTime() < self[ONLOAD] + 9999)
+          // Pull until canStart handler approves the start sequence.
+          if(TRUE !== fireCallback[CALL](self, CANSTART))
+            setTimeout(loadSync, 100);
+          else {
+            // Add relevant class to the HTML element.
+            addClass(self[PREFIX] + '-' + LOADING);
+
+            // Start loading the first chunk of scripts.
+            loadNextChunk(FALSE);
+          }
+        else
+          fireCallback[CALL](self, ONERROR, '*');
+      },
+
+      // Lazy JS exposed implementation.
+      LazyJS = (function() {
+        var LazyJS = function(config) {
+              var self = this, key, value, length, configCopy, loop, pos;
+
+              // Make sure that new LazyJS() is always used.
+              if(!(self instanceof LazyJS))
+                throw 'LazyJS must be instantiated using the new operator.';
+
+              if(config) {
+                if(!isArray(config) || 0 === config[LENGTH] || config[LENGTH] % 2)
+                  throw 'Configuration must be an Array holding even number of entries.';
+
+                // Clone original Array.
+                configCopy = cloneArray(config);
+              }
+
+              // Define the prefix used for class identifiers on the HTML element.
+              self[PREFIX] = 'lazy';
+
+              // Queue of scripts to load.
+              self[QUEUE] = [];
+
+              // Object holding the handlers.
+              self[HANDLERS] = {};
+
+              // Default handler to call before Lazy JS starts loading scripts.
+              self[HANDLERS][CANSTART] = defaultStartHandler;
+
+              // Default handler to call when a script has loaded successfully.
+              self[HANDLERS][ONPROGRESS] = noop;
+
+              // Default handler to call when a chunk of scripts has loaded successfully.
+              self[HANDLERS][ONCHUNK] = noop;
+
+              // Default handler when all scripts have been loaded successfully.
+              self[HANDLERS][ONSUCCESS] = noop;
+
+              // Default handler when an error is detected while loading any of the scripts.
+              self[HANDLERS][ONERROR] = defaultErrorHandler;
+
+              // Go over list of keys and check whether scripts have been defined.
+              if(configCopy) {
+                length = configCopy[LENGTH];
+                for(loop = 0; length > loop;) {
+                  key = configCopy[loop++];
+                  value = configCopy[loop++];
+
+                  // Process the meta-data.
+                  switch(key) {
+                    // These might be defined in the HTML code.
+                    case 'can-start':
+                    case 'on-progress':
+                    case 'on-chunk':
+                    case 'on-success':
+                    case 'on-error':
+                      // Make the variable point to the callable function name.
+                      pos = key.indexOf('-');
+                      key = key[SUBSTR](0, pos) + key.charAt(++pos).toUpperCase() + key[SUBSTR](++pos, 7);
+
+                      // No break here!
+                    case CANSTART:
+                    case ONPROGRESS:
+                    case ONCHUNK:
+                    case ONSUCCESS:
+                    case ONERROR:
+                      // This will set the appropriate handler.
+                      self[key](value);
+                      break;
+
+                    case PREFIX:
+                      self[key] = value;
+                      break;
+
+                    default:
+                      // Default behavior is to add the list of scripts. Scripts must be
+                      // separated by a space character if more than one is listed.
+                      if(!isArray(value))
+                        value = value.split(' ');
+
+                      // Add the scripts along with their identifier.
+                      self[ADD](key, value);
+                  }
+                }
+                configCopy = NULL;
+              }
+            },
+
+            LazyJSPrototype = LazyJS[PROTOTYPE];
+
+        // Define the prototype of Lazy JS.
+
+        // Default handler before Lazy JS starts loading scripts.
+        LazyJSPrototype[CANSTART] = function(handler) {
+          if(!handler)
+            return this[HANDLERS][CANSTART];
+          return (this[HANDLERS][CANSTART] = handler);
+        };
+
+        // Define a callback when a script has loaded successfully.
+        LazyJSPrototype[ONPROGRESS] = function(handler) {
+          if(!handler)
+            return this[HANDLERS][ONPROGRESS];
+          return (this[HANDLERS][ONPROGRESS] = handler);
+        };
+
+        // Define a callback when a chunk of scripts has loaded successfully.
+        LazyJSPrototype[ONCHUNK] = function(handler) {
+          if(!handler)
+            return this[HANDLERS][ONCHUNK];
+          return (this[HANDLERS][ONCHUNK] = handler);
+        };
+
+        // Define a callback when all scripts have been loaded successfully.
+        LazyJSPrototype[ONSUCCESS] = function(handler) {
+          if(!handler)
+            return this[HANDLERS][ONSUCCESS];
+          return (this[HANDLERS][ONSUCCESS] = handler);
+        };
+
+        // Define a callback when an error is detected while loading any of the scripts.
+        LazyJSPrototype[ONERROR] = function(handler) {
+          if(!handler)
+            return this[HANDLERS][ONERROR];
+          return (this[HANDLERS][ONERROR] = handler);
+        };
+
+        // Add a list of scripts to load asynchronously.
+        LazyJSPrototype[ADD] = function(chunkId, scripts) {
+          if(!isArray(scripts))
+            scripts = [scripts];
+          if(scripts && scripts[LENGTH])
+            this[QUEUE][PUSH]([chunkId, scripts]);
+        };
+
+        // Fire-off the loading process and start loading all enqueued scripts!
+        LazyJSPrototype[LOAD] = function() {
+          var self = this;
+          if(!self[QUEUE][LENGTH])
+            // Queue is empty... Run the success handler.
+            self[HANDLERS][ONSUCCESS]();
+          else if(!self[ONLOAD]) {
+            // Mark start time for the loading process.
+            self[ONLOAD] = getTime();
+
+            // Load chunks synchronously.
+            loadSync[CALL](self);
+          }
+        };
+
+        return LazyJS;
+      })();
+
+  // Return the exposed Lazy JS implementation.
+  return LazyJS;
+})(window, Object, Array, Date, null);
